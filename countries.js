6eddc448a7d8f8a1bc5f3ee4363f54957df3f6ef
safinxr <safinxr@gmail.com>
2022-02-23 14:57:07 +0600

    country

--- countries.js
+++ countries.js
@@ -7,10 +7,33 @@ loadCountries();
 const display=data=>{
     const div = document.getElementById("countries");
     data.forEach(country =>{
-        console.log(country);
-        const h2 = document.createElement("h2");
-        h2.innerText=`Name: ${country.name}
-        Capital: ${country.capital}`;
-        div.appendChild(h2);
+        const div2= document.createElement("div");
+        div2.innerHTML =`
+        <h3 class="in">Name: ${country.name}</h3>
+        <p class="in">Capital: ${country.capital}
+        <br><br>
+        <button onclick="loadcountry('${country.name}')">Details</button>
+        `;
+        div2.classList.add("div2");
+        div.appendChild(div2);
     })
+}
+
+const loadcountry= names =>{
+    const details =`https://restcountries.com/v3.1/name/${names}`
+    fetch(details)
+    .then(response =>response.json())
+    .then(countryDetails =>countryF(countryDetails[0]))
+}
+
+const countryF = x =>{
+    console.log(x);
+    const div = document.getElementById("counrtyD");
+    div.innerHTML=`
+    <h4>Name: ${x.name.common}</h4>
+    <p>Popopulation: ${x.population}</p>
+    <p>region: ${x.region}</p>
+    <img src="${x.flags.png}">
+    `
+
 }
